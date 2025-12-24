828302ff4efeb5b12561af8bb0b3f62f433dedf8
terrablue <102580937+terrablue@users.noreply.github.com>
2025-12-24 18:11:46 +0100

    add i18n

---
+++ i18n/i18n.go
@@ -0,0 +1,34 @@
+package i18n
+
+import (
+	"encoding/json"
+	"syscall/js"
+
+	"github.com/primate-run/go/core"
+)
+
+type Dict = core.Dict
+type LocaleAccessor struct{}
+
+func get() js.Value {
+	return js.Global().Get("PRMT_I18N")
+}
+
+func T(key string, params ...Dict) string {
+	i18n := get()
+	if len(params) == 0 || params[0] == nil {
+		return i18n.Get("t").Invoke(key).String()
+	}
+	serialized, _ := json.Marshal(params[0])
+	return i18n.Get("t").Invoke(key, string(serialized)).String()
+}
+
+func (LocaleAccessor) Get() string {
+	return get().Get("locale").String()
+}
+
+func (LocaleAccessor) Set(locale string) {
+	get().Get("set").Invoke(locale)
+}
+
+var Locale LocaleAccessor
